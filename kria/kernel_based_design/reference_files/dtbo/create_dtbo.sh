ae7a90d3fd5f78940cf8ad78eb79755dfb6253fe
FrankKesel <frank.kesel@hs-pforzheim.de>
2026-03-26 16:24:50 +0100

    Changed vadd dtbo script

--- kria/kernel_based_design/reference_files/dtbo/create_dtbo.sh
+++ kria/kernel_based_design/reference_files/dtbo/create_dtbo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Create device tree dtbo"
 
-source /opt/xilinx/2025.1/Vitis/settings64.shxsct dts.tcl
+source /opt/xilinx/2025.1/Vitis/settings64.sh
+xsct dts.tcl
 dtc -@ -O dtb -o pl.dtbo devicetree/devicetree/kv260_platform/psu_cortexa53_0/device_tree_domain/bsp/pl.dtsi
